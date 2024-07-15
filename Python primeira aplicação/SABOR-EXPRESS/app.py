c73efbbd8001abefc3cf6102b86d829cacd09f1a
pontesvjp <pontes.vjp@gmail.com>
2024-07-15 15:26:52 -0300

    manipulacoes de strings

---
+++ Python primeira aplicação/SABOR-EXPRESS/app.py
@@ -0,0 +1,9 @@
+print('Sabor Express')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair')
+
+opcao_escolhida = input('Escolha uma opção: ')
+print(f'voce escolheu a opcao {opcao_escolhida}')
--- Python primeira aplicação/SBOR-EXPRESS/app.py
+++
@@ -1,2 +0,0 @@
-print('Sabor Express')
-
